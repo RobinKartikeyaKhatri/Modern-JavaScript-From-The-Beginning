df33366a3a76544f8eee35e102d4d5ed3e8f5bbd
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-28 15:55:25 +0530

    Learned how to create Object, how to create array of objects, and some operations on objects

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,76 +1,45 @@
-// Arrays and it's methods
+// Object Literals
 
-// Create some arrays
-const numbers = [43, 56, 33, 23, 44, 36, 5];
-const numbers2 = new Array(22, 45, 33, 76, 54);
-const fruit = ["Apple", "Banana", "Orange", "Pear"];
-const mixed = [22, "Hello", true, undefined, null, {a: 1, b: 1}, new Date()];
+const person = {
+    firstName: "Steve",
+    lastName: "Smith",
+    age: 30,
+    email: "steve@aol.com",
+    hobbies: ["music", "sports"],
+    address: {
+        city: "Miami",
+        state: "FL"
+    },
+    getBirthYear: function () {
+        return 2019 - this.age;
+    }
+}
 
 let val;
 
-// Get array length
-val = numbers.length;
+val = person;
 
-// Check if is array
-val = Array.isArray(numbers);
+// Get specific value
+val = person.firstName;
+val = person["lastName"];
+val = person.age;
+val = person.hobbies[1];
+val = person.address.city;
+val = person.address["state"];
+val = person.getBirthYear();
 
-// Get a single value from array
-val = numbers[3];
-
-// Insert into array
-numbers[2] = 100;
-
-// Find index of value
-val = numbers.indexOf(36);
-
-
-
-
-// Mutating arrays
-
-// Add on to end
-// numbers.push(250);
-
-// Add on to front
-// numbers.unshift(120);
-
-// take off from end
-// numbers.pop();
-
-// take off from front
-// numbers.shift();
-
-// Splice values
-// numbers.splice(1, 1);
-
-// Reverse array
-// numbers.reverse();
-
-
-
-// Concatenate arrays
-val = numbers.concat(numbers2);
-
-// Sort arrays
-val = fruit.sort();
-// val = numbers.sort();
-
-// use the compare function
-// val = numbers.sort(function (x, y) {
-//     return x > y;
-// });
-
-// // Reverse sort
-// val = numbers.sort(function (x, y) {
-//     return x < y;
-// });
-
-// Find
-function under50(num) {
-    return num < 50;
-}
-
-val = numbers.find(under50);
-
-console.log(numbers);
 console.log(val);
+
+
+
+// Array of Object
+const people = [
+    {name: "John", age: 30},
+    {name: "Mike", age: 23},
+    {name: "Nancy", age: 40}
+];
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+    console.log(people[i].age);
+}

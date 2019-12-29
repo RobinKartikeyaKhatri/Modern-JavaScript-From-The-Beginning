d9ac9d1f0c5e0f6f4daecbadee2c1504e5541f1f
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-29 13:05:38 +0530

    Learned about Date and its methods

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,45 +1,26 @@
-// Object Literals
-
-const person = {
-    firstName: "Steve",
-    lastName: "Smith",
-    age: 30,
-    email: "steve@aol.com",
-    hobbies: ["music", "sports"],
-    address: {
-        city: "Miami",
-        state: "FL"
-    },
-    getBirthYear: function () {
-        return 2019 - this.age;
-    }
-}
-
+// Dates and Times
 let val;
 
-val = person;
+const today = new Date();
+let birthday = new Date('9-10-1981 11:25:00');
+birthday = new Date('September 10 1981');
+birthday = new Date('9/10/1981');
 
-// Get specific value
-val = person.firstName;
-val = person["lastName"];
-val = person.age;
-val = person.hobbies[1];
-val = person.address.city;
-val = person.address["state"];
-val = person.getBirthYear();
+val = today.getMonth();
+val = today.getDate();
+val = today.getDay();
+val = today.getFullYear();
+val = today.getHours();
+val = today.getMinutes();
+val = today.getSeconds();
+val = today.getMilliseconds();
+val = today.getTime();
 
-console.log(val);
+birthday.setMonth(2);
+birthday.setDate(12);
+birthday.setFullYear(1985);
+birthday.setHours(3);
+birthday.setMinutes(30);
+birthday.setSeconds(25);
 
-
-
-// Array of Object
-const people = [
-    {name: "John", age: 30},
-    {name: "Mike", age: 23},
-    {name: "Nancy", age: 40}
-];
-
-for (let i = 0; i < people.length; i++) {
-    console.log(people[i].name);
-    console.log(people[i].age);
-}
+console.log(birthday);

fc19f83d31e782ce2789a01b66de31af653c8ee8
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2019-12-31 13:09:33 +0530

    Learn how to select multiple elements in DOM

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,39 +1,53 @@
-// document.getElementById()
+// document.getElementsByClassName()
 
-// console.log(document.getElementById("task-title"));
+// const items = document.getElementsByClassName("collection-item");
+// console.log(items);
+// console.log(items[0]);
+// items[0].style.color = "red";
+// items[3].textContent = "Hello World";
 
-// // Get things from the element
-// console.log(document.getElementById("task-title").id);
-// console.log(document.getElementById("task-title").className);
+// const listItems = document.querySelector("ul").getElementsByClassName("collection-item");
 
-// const taskTitle = document.getElementById("task-title");
+// console.log(listItems);
 
-// // Change Styling
-// taskTitle.style.background = "#333";
-// taskTitle.style.color = "#fff";
-// taskTitle.style.padding = "5px";
-// // taskTitle.style.display = "none";
+// document.getElementsByTagName()
+// let lis = document.getElementsByTagName("li");
+// console.log(lis);
+// console.log(lis[0]);
+// lis[0].style.color = "red";
+// lis[3].textContent = "Hello World";
 
-// // Change Content
-// taskTitle.textContent = "Task List";
-// taskTitle.innerText = "My Tasks";
-// taskTitle.innerHTML = "<span style='color:red;'>Task List</span>"
+// // Convert HTML Collection into array
+// lis = Array.from(lis);
 
+// lis.reverse();
 
+// lis.forEach(function(li, index) {
+//     console.log(li.className);
+//     li.textContent = `${index}: Hello`;
+// });
 
-// document.querySelector()
-console.log(document.querySelector("#task-title"));
-console.log(document.querySelector(".card-title"));
-console.log(document.querySelector("h5"));
-
-document.querySelector("li").style.color = "red";
-document.querySelector("ul li").style.color = "blue";
-
-document.querySelector("li:last-child").style.color = "yellow";
-document.querySelector("li:nth-child(3)").style.color = "pink";
-document.querySelector("li:nth-child(4)").textContent = "Hello World";
-document.querySelector("li:nth-child(odd)").style.background = "#ccc";
-document.querySelector("li:nth-child(even)").style.background = "#f4f4f4";
+// console.log(lis);
 
 
 
+
+// document.querySelectorAll()
+const items = document.querySelectorAll("ul.collection li.collection-item");
+
+items.forEach(function(item, index) {
+    item.textContent = `${index}: Hello`;
+});
+
+const liOdd = document.querySelectorAll("li:nth-child(odd)");
+const liEven = document.querySelectorAll("li:nth-child(even)");
+
+liOdd.forEach(function(li, index) {
+    li.style.background = "#ccc";
+});
+
+for (let i = 0; i < liEven.length; i++) {
+    liEven[i].style.background = "#f1f1f1";
+}
+
+console.log(items);

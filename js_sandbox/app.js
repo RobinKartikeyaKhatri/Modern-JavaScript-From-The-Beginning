6fb3bfb78e309363e96497924de890fd648ae9f9
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-01-01 09:29:47 +0530

    Learn how to traverse DOM with builtin JavaScript properties

--- js_sandbox/app.js
+++ js_sandbox/app.js
@@ -1,53 +1,57 @@
-// document.getElementsByClassName()
+let val;
 
-// const items = document.getElementsByClassName("collection-item");
-// console.log(items);
-// console.log(items[0]);
-// items[0].style.color = "red";
-// items[3].textContent = "Hello World";
+const list = document.querySelector("ul.collection");
+const listItem = document.querySelector("li.collection-item:first-child");
 
-// const listItems = document.querySelector("ul").getElementsByClassName("collection-item");
-
-// console.log(listItems);
-
-// document.getElementsByTagName()
-// let lis = document.getElementsByTagName("li");
-// console.log(lis);
-// console.log(lis[0]);
-// lis[0].style.color = "red";
-// lis[3].textContent = "Hello World";
-
-// // Convert HTML Collection into array
-// lis = Array.from(lis);
-
-// lis.reverse();
-
-// lis.forEach(function(li, index) {
-//     console.log(li.className);
-//     li.textContent = `${index}: Hello`;
-// });
-
-// console.log(lis);
+val = listItem;
+val = list;
 
 
+// Get child nodes
+val = list.childNodes;
+val = list.childNodes[0];
+val = list.childNodes[0].nodeName;
+val = list.childNodes[0].nodeType;
+// 1 - Element
+// 2 - Attribute (deprecated)
+// 3 - Text node
+// 8 - Comment
+// 9 - Document itself
+// 10 - Doctype
+
+// Get children element nodes
+val = list.children;
+val = list.children[1];
+list.children[1].textContent = "Hello";
+
+// Children of children
+list.children[3].children[0].id = "test-link";
+val = list.children[3].children[0];
+
+// First child
+val = list.firstChild;
+val = list.firstElementChild;
+
+// Last child
+val = list.lastChild;
+val = list.lastElementChild;
+
+// Count child elements
+val = list.childElementCount;
 
 
-// document.querySelectorAll()
-const items = document.querySelectorAll("ul.collection li.collection-item");
+// Get parent node
+val = listItem.parentNode;
+val = listItem.parentElement;
+val = listItem.parentElement.parentElement;
 
-items.forEach(function(item, index) {
-    item.textContent = `${index}: Hello`;
-});
+// Get next sibling
+val = listItem.nextSibling;
+val = listItem.nextElementSibling.nextElementSibling;
 
-const liOdd = document.querySelectorAll("li:nth-child(odd)");
-const liEven = document.querySelectorAll("li:nth-child(even)");
 
-liOdd.forEach(function(li, index) {
-    li.style.background = "#ccc";
-});
+// Get previous sbling
+val = listItem.previousSibling;
+val = listItem.previousElementSibling;
 
-for (let i = 0; i < liEven.length; i++) {
-    liEven[i].style.background = "#f1f1f1";
-}
-
-console.log(items);
+console.log(val);

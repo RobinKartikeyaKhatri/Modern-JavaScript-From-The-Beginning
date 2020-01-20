284dc7dd497fba83f9bd46938c134e61479a5842
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-01-20 13:56:29 +0530

    Learn about Promises

--- callbackFunctions/app.js
+++ callbackFunctions/app.js
@@ -24,11 +24,20 @@ const posts = [
 // getPosts();
 
 
-function createPost(post, callback) {
-    setTimeout(function() {
-        posts.push(post);
-        callback();
-    }, 2000);
+function createPost(post) {
+    return new Promise(function(resolve, reject){
+        setTimeout(function() {
+            posts.push(post);
+
+            const error = false;
+
+            if (!error) {
+                resolve();
+            } else {
+                reject("Error: Something went wrong");
+            }
+        }, 2000);
+    });
 }
 
 function getPosts() {
@@ -41,4 +50,8 @@ function getPosts() {
     }, 1000);
 }
 
-createPost({title: "Post Three", body: "This is post three"}, getPosts);
+createPost({title: "Post Three", body: "This is post three"})
+.then(getPosts)
+.catch(function(err){
+    console.log(err);
+});

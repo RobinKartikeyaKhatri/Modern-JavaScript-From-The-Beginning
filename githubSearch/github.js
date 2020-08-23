c40297a3c10efa586a897e6b49942f2bdaf8cafc
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-08-23 15:57:05 +0530

    Create a method showRepos() to fetch user repos details

--- githubSearch/github.js
+++ githubSearch/github.js
@@ -2,15 +2,22 @@ class Github {
     constructor() {
         this.client_id = "f14b0e5bf12095809c0c",
         this.client_secret = "a3705d74170f09e8e917855686d98f0f0056b638";
+        this.repos_count = 5;
+        this.repos_sort = "created_asc";
     }
 
     async getUser(user) {
         const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
+        const reposResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
         const profile = await profileResponse.json();
+        const repos = await reposResponse.json();
+        
 
         return {
-            profile
+            profile,
+            repos
         }
     }
 }

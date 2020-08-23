8ccfc471b4e202278ebbd339ec3c30e96a0272d6
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-08-23 14:12:17 +0530

    Create Github class and method getUser() to fetch user profile data, called getUser() method inside app.js

--- githubSearch/github.js
+++ githubSearch/github.js
@@ -1,6 +1,16 @@
 class Github {
     constructor() {
         this.client_id = "f14b0e5bf12095809c0c",
-        this.client_secret = "a3705d74170f09e8e917855686d98f0f0056b638"
+        this.client_secret = "a3705d74170f09e8e917855686d98f0f0056b638";
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        return {
+            profile
+        }
     }
 }

cf65a1322d2d2e3ffc5fe9959fee77b64575d8ac
Robin Kartikeya Khatri <kartikrobin@gmail.com>
2020-08-24 13:46:45 +0530

    Learned about metacharacter symbols in regular expressions

--- regularExpressions/app.js
+++ regularExpressions/app.js
@@ -1,39 +1,31 @@
 let re;
-re = /hello/
-re = /hello/i; // i = case insensitive
-// re = /hello/g; // Global search
+// Literal Characters
+re = /hello/;
+re = /hello/i;
 
-// console.log(re);
-// console.log(re.source);
+// Metacharacter Symbols
+re = /^h/i;             // Must start with h
+re = /d$/i;             // Must ends with d
+re = /^hello$/i         // Must begin and end with
+re = /h.llo/i           // Matches any ONE character
+re = /h*llo/i;          // Matches any character 0 or more times
+re = /gre?a?y/i;        // Optional character
+re = /gre?a?y\?/i;         // Escape characters
 
+// String to match
+const str = "Gray?";
 
-// exec() - Return result in an array or null
-// const result = re.exec('brad hello world');
+// Log results
+const result = re.exec(str);
+console.log(result);
 
-// console.log(result);
-// console.log(result[0]);
-// console.log(result.index);
-// console.log(result.input);
-
-
-// test() - Returns true or false
-// const result = re.test("Hello");
-// console.log(result);
-
-
-// match() - Return result array or null
-// const str = "Hello There";
-// const result = str.match(re);
-// console.log(result);
-
-
-// search() - Returns the index of first match if not found returns -1
-// const str = "Brad Hello There";
-// const result = str.search(re);
-// console.log(result);
+function reTest(re, str) {
+    if (re.test(str)) {
+        console.log(`${str} matches ${re.source}`);
+    } else {
+        console.log(`${str} does NOT matches ${re.source}`)
+    }
+}
 
 
-// replace() - Return a new string with some or all matches of a pattern
-const str = "Hello There";
-const newStr = str.replace(re, "Hi");
-console.log(newStr);
+reTest(re, str);
